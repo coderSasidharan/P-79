5b39c2d91a3bfa70dbfbf2c9365bad7802e93d5f
coderSasidharan <66076670+coderSasidharan@users.noreply.github.com>
2021-02-16 13:26:47 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+require('@firebase/firestore')
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDbiWCit7XRVKJIpUD7tSyRKXEmWRvB5GA",
+  authDomain: "barterapp-d1b4b.firebaseapp.com",
+  projectId: "barterapp-d1b4b",
+  storageBucket: "barterapp-d1b4b.appspot.com",
+  messagingSenderId: "205662274945",
+  appId: "1:205662274945:web:344b51d98f1dd6f9dcd480"
+};
+  // Initialize Firebase
+
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase.firestore();
